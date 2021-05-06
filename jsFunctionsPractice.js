da0657e7649fe231ca46eb37e1bedd90ddeeedca
DomBurke23 <dominiqueburke@hotmail.co.uk>
2021-05-06 11:43:30 +0100

    Update jsFunctionsPractice.js

--- jsFunctionsPractice.js
+++ jsFunctionsPractice.js
@@ -81,3 +81,12 @@ function logCitySkyline() { // code inside {} is a block
   return 'The stars over the ' + skyscraper + ' in ' + city; 
 }
 console.log(logCitySkyline());
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+const callMyNightSky = () => {
+  stars = 'Sirius';
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+console.log(callMyNightSky());
+console.log(stars); // prints the local value not global 
